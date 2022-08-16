350e949cb83030a44575a94bcf0547eebc391a72
Altan Çelik <celik.altann@gmail.com>
2022-08-17 00:20:28 +0300

    Bölüm 6 Bitti

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import logo from 'react';
 import Category from './Category';
 import Navi from './navi';
 import ProductList from './ProductList';
+import {Container, Row, Col} from "reactstrap"
 
 function App() {
   return (
     <div>
-      <Navi></Navi>
-      <Category></Category>
-      <ProductList></ProductList>
+      <Container>
+         <Row>
+            <Navi />
+         </Row>
+         <Row>
+            <Col xs="3">
+               <Category /> 
+            </Col>
+            <Col xs="9">
+              <ProductList />
+            </Col>
+         </Row>
+      </Container>
+ 
     </div>
   );
 }
--- src/Category.js
+++ src/Category.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import {ListGroup,ListGroupItem} from 'reactstrap'
 
 export default class Category extends Component {
   render() {
     return (
       <div>
-        <h3>CategoryLİst</h3>
+        <h3>CategoryLst</h3>
+        <ListGroup>
+  <ListGroupItem>
+    Cras justo odio
+  </ListGroupItem>
+  <ListGroupItem active>
+    Dapibus ac facilisis in
+  </ListGroupItem>
+  <ListGroupItem>
+    Morbi leo risus
+  </ListGroupItem>
+  <ListGroupItem>
+    Porta ac consectetur ac
+  </ListGroupItem>
+  <ListGroupItem>
+    Vestibulum at eros
+  </ListGroupItem>
+</ListGroup>
       </div>
     )
   }
